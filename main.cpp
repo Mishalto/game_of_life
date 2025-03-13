bce3c073ee3c31decd4a6ef769cb2606d8c2f6f3
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-13 21:22:55 +0300

    Create spawn cell logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <array>
 #include <vector>
+#include <utility>
 #include <SFML/Graphics.hpp>
 
 constexpr int width = 1920;
@@ -10,14 +10,22 @@ constexpr int cell_size = 20;
 constexpr int x_cells = width / cell_size;
 constexpr int y_cells = height / cell_size;
 
+void set_start_pos(std::vector<std::vector<sf::RectangleShape>>& grid) {
+    std::vector<std::pair<int, int>> start_pos = {{28, 42}, {29, 42}, {30, 42}, {31, 42}, {32, 42}};
+    for (const auto& s : start_pos) {
+        grid[s.first][s.second].setFillColor(sf::Color::Cyan);
+    }
+}
+
 void init_grid(std::vector<std::vector<sf::RectangleShape>>& grid) {
     for (size_t i = 0; i < grid.size(); ++i) {
         for (size_t j = 0; j < grid[i].size(); ++j) {
-            grid[i][j].setFillColor(sf::Color::White);
+            grid[i][j].setFillColor(sf::Color::Black);
             grid[i][j].setSize({cell_size, cell_size});
             grid[i][j].setPosition({static_cast<float>(j) * cell_size, static_cast<float>(i) * cell_size});
         }
     }
+    set_start_pos(grid);
 }
 
 int main() {

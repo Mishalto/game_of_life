efb35678075f7eb9c9cd5ccb5c1fc923ae91c432
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-17 18:22:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,8 +37,8 @@ int main() {
     init_grid(grid);
     set_start_pos(grid);
     std::vector<std::vector<sf::RectangleShape>> next_grid(y_cells, std::vector<sf::RectangleShape>(x_cells));
+    init_grid(next_grid);
     set_start_pos(next_grid);
-    bool swapped = false;
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -46,8 +46,57 @@ int main() {
                 window.close();
             }
         }
-        if (swapped) {
-            
+
+        for (size_t i = 0; i < grid.size(); ++i) {
+            for (size_t j = 0; j < grid[i].size(); ++j) {
+                int living_cells = 0;
+
+                // Top
+                if (i - 1 >= 0) {
+                    if (j - 1 >= 0 && grid[i - 1][j - 1].getFillColor() == sf::Color::Cyan) {
+                        living_cells += 1;
+                    }
+                    if (grid[i - 1][j].getFillColor() == sf::Color::Cyan) {
+                        living_cells += 1;
+                    }
+                    if (j + 1 < grid[i].size() && grid[i - 1][j + 1].getFillColor() == sf::Color::Cyan) {
+                        living_cells += 1;
+                    }
+                }
+                // Middle
+                if (j - 1 >= 0 && grid[i][j - 1].getFillColor() == sf::Color::Cyan) {
+                    living_cells += 1;
+                }
+                if (grid[i][j].getFillColor() == sf::Color::Cyan) {
+                    living_cells += 1;
+                }
+                if (j + 1 < grid[i].size() && grid[i][j + 1].getFillColor() == sf::Color::Cyan) {
+                    living_cells += 1;
+                }
+                // Bottom
+                if (i + 1 < grid.size()) {
+                    if (j - 1 >= 0 && grid[i + 1][j - 1].getFillColor() == sf::Color::Cyan) {
+                        living_cells += 1;
+                    }
+                    if (grid[i + 1][j].getFillColor() == sf::Color::Cyan) {
+                        living_cells += 1;
+                    }
+                    if (j + 1 < grid[i].size() && grid[i + 1][j + 1].getFillColor() == sf::Color::Cyan) {
+                        living_cells += 1;
+                    }
+                }
+
+                // Condition
+                if (grid[i][j].getFillColor() == sf::Color::Black && living_cells == 3) {
+                    next_grid[i][j].setFillColor(sf::Color::Cyan);
+                }
+                if (grid[i][j].getFillColor() == sf::Color::Cyan && living_cells < 2) {
+                    next_grid[i][j].setFillColor(sf::Color::Black);
+                }
+                if (grid[i][j].getFillColor() == sf::Color::Cyan && living_cells > 3) {
+                    next_grid[i][j].setFillColor(sf::Color::Black);
+                }
+            }
         }
 
         window.clear();
@@ -57,6 +106,13 @@ int main() {
             }
         }
         window.display();
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        for (size_t i = 0; i < grid.size(); ++i) {
+            for (size_t j = 0; j < grid[i].size(); ++j) {
+                next_grid[i][j] = grid[i][j];
+            }
+        }
     }
 }
 
